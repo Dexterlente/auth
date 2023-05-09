77dc780e70d12c12a914b857a23da14b52bb6f7d
Dexterlente <dexter.lente18@gmail.com>
2023-05-09 23:05:45 +0800

    D

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # all auth urls because it was using all auth
+    path('accounts/', include('allauth.urls'))
 ]
