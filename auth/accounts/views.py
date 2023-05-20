17885505bd8220431bf15480268649d223507dca
Dexterlente <dexter.lente18@gmail.com>
2023-05-20 19:41:15 +0800

    d

--- auth/accounts/views.py
+++ auth/accounts/views.py
@@ -6,7 +6,7 @@ from dj_rest_auth.social_serializers import TwitterLoginSerializer
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
-
+# checkendpoints
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
 
