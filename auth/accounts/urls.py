d74abbe0cd04be032839ed277b4b0774ee58e91b
Dexterlente <dexter.lente18@gmail.com>
2023-05-24 01:22:38 +0800

    D

--- auth/accounts/urls.py
+++ auth/accounts/urls.py
@@ -17,5 +17,14 @@ urlpatterns = [
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('dj-rest-auth/facebook/', views.FacebookLogin.as_view(), name='fb_login'),
     path('dj-rest-auth/twitter/', views.TwitterLogin.as_view(), name='twitter_login'),
-    path('dj-rest-auth/google/', views.GoogleLogin.as_view(), name='google_login')
+    path('dj-rest-auth/google/', views.GoogleLogin.as_view(), name='google_login'),
+
+    # docs
+    # path('password/reset/', PasswordResetView.as_view(), name='rest_password_reset'),
+    # path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
+    # path('login/', LoginView.as_view(), name='rest_login'),
+    # # URLs that require a user to be logged in with a valid session / token.
+    # path('logout/', LogoutView.as_view(), name='rest_logout'),
+    # path('user/', UserDetailsView.as_view(), name='rest_user_details'),
+    # path('password/change/', PasswordChangeView.as_view(), name='rest_password_change'),
 ]
