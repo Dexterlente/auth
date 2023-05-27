a49f8c42ab2ac5239cbcff70c15a3aa5b4642610
Dexterlente <dexter.lente18@gmail.com>
2023-05-27 22:42:10 +0800

    D

--- auth/accounts/views.py
+++ auth/accounts/views.py
@@ -7,7 +7,54 @@ from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from rest_framework.permissions import AllowAny
+from django.views.decorators.debug import sensitive_post_parameters
+from django.utils.decorators import method_decorator
+from dj_rest_auth.registration.views import RegisterView, VerifyEmailView
 
+sensitive_post_parameters_m = method_decorator(
+    sensitive_post_parameters("password1", "password2")
+)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class RegisterAPIView(RegisterView):
+    permission_classes = [AllowAny]
+
+    @sensitive_post_parameters_m
+    def dispatch(self, *args, **kwargs):
+        return super(RegisterAPIView, self).dispatch(*args, **kwargs)
+
+    def get_response_data(self, user):
+        if getattr(settings, "REST_USE_JWT", False):
+            data = {"user": user, "token": self.token}
+        return JWTSerializer(data).data
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(
+            self.get_response_data(user),
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
+
+    def perform_create(self, serializer):
+        user = serializer.save(self.request)
+        if getattr(settings, "REST_USE_JWT", False):
+            self.token = jwt_encode(user)
+
+        email = EmailAddress.objects.get(email=user.email, user=user)
+        confirmation = EmailConfirmationHMAC(email)
+        key = confirmation.key
+        # TODO Send mail confirmation here .
+        # send_register_mail.delay(user, key)
+        print("account-confirm-email/" + key)
+        return user
+        
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
 
